18fc6f0b50a0d717065cabf9c68e2c8ec56fb809
codewithmesree <sreedweasley@gmail.com>
2026-06-16 10:55:53 +0530

    made package changes

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="card">
+        <div className="card-content">
+          <h1 className="title">TrackFlow</h1>
+          <p className="subtitle">
+            Coming Soon!
+          </p>
+        </div>
+
+        <div className="card-footer">
+          <span>Edit</span>
+          <code className="code-badge">src/App.jsx</code>
+          <span>to see changes instantly.</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
